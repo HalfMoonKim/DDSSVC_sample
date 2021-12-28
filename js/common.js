9c1a1dbd7f3dbe2f9b4f01969e39fa9fbb863204
HalfmoonKim <shlove0602@kakao.com>
2021-12-28 17:15:06 +0900

    21.12.28. 작업

--- js/common.js
+++ js/common.js
@@ -2,26 +2,27 @@ $(document).ready(function () {
     // Login Raido Btn 기능
     $('input[name=group]').click(function () {
         let groupVal = $('input[name=group]:checked').val();
+        let loginForm = $('.login-form');
 
         if (groupVal === "erp") {
-            $('.main__login-form').attr("action", "./Home.html")
+            loginForm.attr("action", "./ERP.html");
         } else if (groupVal === "service") {
-            $('.main__login-form').attr("action", "./Service.html")
+            loginForm.attr("action", "./Service.html");
         } else if (groupVal === "userPay") {
-            $('.main__login-form').attr("action", "./UserPay.html")
+            loginForm.attr("action", "./UserPay.html");
         };
     });
 
-    // Home Nav 기능
-    const homeMenu = new Swiper('.home-menu-slide', {
-        loop: false,
+    // ERP Nav 기능
+    const erpMenu = new Swiper('.erp-menu-slide', {
         slidesPerView: 4,
         slidesPerGroup: 4,
         spaceBetween: 30,
+        touchRatio: 0,
 
         pagination: {
             el: '.swiper-pagination',
-            clickable: true
+            clickable: true,
         },
 
         navigation: {
@@ -30,15 +31,32 @@ $(document).ready(function () {
         },
     });
 
+    let tree = $('.tree');
+
+    tree.each(function () {
+        $(this).find('li').each(function () {
+            $(this).append('<span></span>');
+            if (!$(this).children('ul').length) {
+                $(this).addClass('final');
+            } else if ($(this).is(':last-child')) {
+                $(this).addClass('last');
+            }
+        });
+        $(this).find('button').on('click', function () {
+            if ($(this).parent('li').hasClass('unfold')) {
+                $(this).parent('li').removeClass('unfold');
+            } else {
+                $(this).parent('li').addClass('unfold');
+            }
+        });
+    });
+
     for (let i = 0; i <= $('.tree-open').length; i++) {
-        // Tree Modal Open
+        let treeCont = $('.tree-content');
+
         $('.tree-open').eq(i).click(function () {
-            $('.tree-list').stop().hide();
-            $('.tree-list').eq(i).stop().fadeIn();
+            treeCont.stop().hide();
+            treeCont.eq(i).stop().fadeIn();
         });
-        // Tree Modal Close
-        $('.tree-close').eq(i).click(function () {
-            $('.tree-box').eq(i).stop().fadeOut();
-        });
-    }
+    };
 });
